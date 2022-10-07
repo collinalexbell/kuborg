b1f649b3008c0fff86da065c25439f0fbf6200bf
kuberlog <collinalexbell@gmail.com>
2022-10-07 23:13:52 +0000

    package and interfaces

---
+++ interfaces/console.js
@@ -0,0 +1,11 @@
+// index.js
+class Console {
+   constructor() {
+   } 
+   usage() {
+    console.log("there isn't any useage as of yet");
+   }
+}
+
+const singleton = new Console();
+singleton.usage();
